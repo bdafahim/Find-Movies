dd23260019f7ceb7851a6b349e8780d3f9f6a18c
bdafahim <bdafahim@gmail.com>
2025-03-27 20:37:18 +0200

    Nav Component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import './App.css'
 import Home from "./pages/Home.jsx";
 import {Route, Routes} from "react-router-dom";
 import Favorites from "./pages/Favorites.jsx";
+import NavBar from "./components/NavBar.jsx";
 function App() {
 
   return (
-    <main className='main-content'>
-        <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/favorites' element={<Favorites/>} />
-        </Routes>
-    </main>
+      <div>
+          <NavBar/>
+          <main className='main-content'>
+              <Routes>
+                  <Route path='/' element={<Home/>}/>
+                  <Route path='/favorites' element={<Favorites/>}/>
+              </Routes>
+          </main>
+      </div>
   )
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import {Link} from "react-router-dom";
+
+function NavBar() {
+    return(
+        <nav className='navbar'>
+            <div className='navbar-brand'>
+                <Link to={'/'}>Movie App</Link>
+            </div>
+            <div className='navbar-links'>
+                <Link className='nav-link' to={'/'}>Home</Link>
+                <Link className='nav-link' to={'/favorites'}>Favorites</Link>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
